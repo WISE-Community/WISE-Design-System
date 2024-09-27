bb21929ce37d20bf6d5459fb429537f0891eefd3
Jonathan Lim-Breitbart <breity10@gmail.com>
2024-09-26 23:12:41 -0700

    feat(Documentation): Use Source block to show raw files - Requires raw-loader (npm install raw-loader --save-dev) - Use react-tabs in Dialog MDX (npm install react-tabs --save-dev)

--- src/app/docs/dialog/dialog.stories.ts
+++ src/app/docs/dialog/dialog.stories.ts
@@ -1,20 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { moduleMetadata } from '@storybook/angular';
 import { CommonModule } from '@angular/common';
-// import { WISEDialogService } from '../../shared/components/actions/dialog/dialog.service';
 import { BasicDialog } from './basic-dialog/basic-dialog.component';
-import { BasicDialogInfo } from './basic-dialog-info/basic-dialog-info.component';
-
-// function dialogServiceFactory(dialogService: WISEDialogService) {
-//   return () => dialogService;
-// }
 
 const meta: Meta = {
   title: 'Components/Actions/Dialog',
   decorators: [
     moduleMetadata({
       declarations: [],
-      imports: [BasicDialog, BasicDialogInfo, CommonModule],
+      imports: [BasicDialog, CommonModule],
     }),
   ],
   argTypes: {},
@@ -27,7 +21,6 @@ export const Dialog: Story = {
   render: () => ({
     template: `
       <basic-dialog></basic-dialog>
-      <basic-dialog-info></basic-dialog-info>
     `,
   }),
 };
